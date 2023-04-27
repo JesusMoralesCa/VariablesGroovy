dde09060bfd5af67d573e915f1debc72b3d3478c
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-04-27 18:14:58 +0200

    Update variables.groovy

--- vars/variables.groovy
+++ vars/variables.groovy
@@ -1,13 +1,27 @@
+def createMavenProject(){
+    def mavenCommand = "mvn archetype:generate -DgroupId=com.tarea.app -DartifactId=hola-mundo -DinteractiveMode=false"
+    sh "${mavenCommand}"
+}
+
+
+
 def check(){
     checkout scm
 }
 
 
 def crPackage(){
-    sh 'mvn clean package'
+    def mavenCommand = "mvn clean package"
+    sh "${mavenCommand}"
 }
 
 
 def testUni{
-    sh 'mvn test'
+    def mavenCommand = "mvn test"
+    sh "${mavenCommand}"
+}
+
+def jacoco{
+    def mavenCommand = "mvn clean test jacoco:report"
+    sh "${mavenCommand}"
 }
